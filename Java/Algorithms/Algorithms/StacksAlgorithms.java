e59e0cf784e3a41bccdde878c3d8fd63e26e771a
neophyteCode44 <157976564+neophyteCode44@users.noreply.github.com>
2024-08-06 08:46:51 +0530

    Add Next Greater Element Algorithm

--- Java/Algorithms/Algorithms/StacksAlgorithms.java
+++ Java/Algorithms/Algorithms/StacksAlgorithms.java
@@ -19,10 +19,52 @@ public class StacksAlgorithms {
 
         //stack.peek(); --> Checking without removing
 
+        //Next Greater Element Algorithm
+        System.out.println("\n Next Greater Element Algorithm \n");
+        printNextGreaterElement(new int[] {15,13,7,2,45});
+
     }
 
     /**
      * Next Greater Element Algorithm:
      */
+    public static void printNextGreaterElement(int[] arr){
+        if(arr.length ==0 ){
+            System.out.println();
+            return;
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(arr[0]);
+
+        for(int i =1 ; i<arr.length;i++){
+            int next = arr[i];
+            //Checking the stack
+            if(!stack.empty()){
+                int popped = stack.pop();
+                while(popped< next){
+                    System.out.println(popped+" --> "+next);
+                    //Updating the popped value
+                    if(stack.empty()){
+                        break;
+                    }
+                    popped = stack.pop();
+                }
+
+                //If popped is greater than  next -> pushing to the stack
+                if(popped> next){
+                    stack.push(popped);
+                }
+
+            }
+            stack.push(next);
+        }
+
+        //After above loop remaining items on the stack doesn't have next greater elements
+        while (!stack.empty()){
+            System.out.println(stack.pop()+" --> "+-1);
+        }
+    }
+
 
 }
